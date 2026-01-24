6b3337b5f244b0a71e8e8855e7858d59b4613e36
JonathanNevarez <neva_rez00@hotmail.com>
2026-01-24 12:20:13 -0500

    SoilNet.1.4.2

--- backend/index.js
+++ backend/index.js
@@ -56,7 +56,7 @@ app.use(cors({
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
-app.options("/*", cors());
+app.options(/.*/, cors());
 
 /* ========================
    SERVIDOR HTTP
